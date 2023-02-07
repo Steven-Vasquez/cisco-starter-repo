eed67c9cdd2538ef75e1ae9e03ecddbef5195e0b
steven vasquez <ministeven10@gmail.com>
2023-02-07 15:56:34 -0800

    modified file structure to give components their own files and stylesheets

--- sextan/src/App.js
+++ sextan/src/App.js
@@ -1,33 +1,18 @@
+import React, {Component} from 'react';
 import './App.css';
-
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
-      <div style={{backgroundColor:"CadetBlue", padding:"1rem"}}>
-        <Banner />
-      </div>
-      <Exhibit 
-        title="Child Components displayed below"/>
+      <Banner bannerText="Connection Tester"/>
+      <Exhibit name="Child Components displayed below"/>
+      <Exhibit name="This is another exhibit"/>
+      <Exhibit name="And another one"/>
     </div>
   );
 }
 
-function Banner() {
-  return (
-    <h2 alt="Website Title">Connection Tester</h2>
-  );
-}
-
-function Exhibit(props){
-  return(
-    <div>
-      <header>
-        <h1>{props.title}</h1>
-      </header>
-      {props.children}
-    </div>
-  )
-}
 
 export default App;
